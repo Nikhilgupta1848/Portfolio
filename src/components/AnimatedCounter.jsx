02c76efa63dd6f67ba59a35c42d23179a0e0d89c
Nikhil Kumar <nikhilkrkanti@gmail.com>
2025-05-21 11:13:11 +0530

    bot unique key value issues are fixed in hero section and Animatedcounter

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -6,9 +6,11 @@ const AnimatedCounter = () => {
     <div id="counter" className="padding-x-lg sl:mt-0 mt-32">
       <div className="mx-auto grid-4-cols">
         {counterItems.map((item) => (
-          <div className="bg-zinc-900 rounded-lg  p-10 flex flex-col justify-center">
+          <div
+          key={item.label}
+          className="bg-zinc-900 rounded-lg  p-10 flex flex-col justify-center">
             <div
-              key={counterItems.label}
+              
               className="counter-number text-white text-5xl font-bold mb-2"
             >
               <CountUp suffix={item.suffix} end={item.value} />
@@ -16,7 +18,7 @@ const AnimatedCounter = () => {
 
             <div className="text-white-50 text-lg">{item.label}</div>
           </div>
-        ))}
+))}
       </div>
     </div>
   );
